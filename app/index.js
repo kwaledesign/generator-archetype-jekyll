ba071595e134ec137321ef537353009df88d70bb
kwaledesign <sam@kwaledesign.com>
2013-09-07 08:28:09 -0600

    integrate jekyll and archetype

--- app/index.js
+++ app/index.js
@@ -1,50 +1,519 @@
 'use strict';
 var util = require('util');
 var path = require('path');
+var spawn = require('child_process').spawn;
+var chalk = require('chalk');
 var yeoman = require('yeoman-generator');
-
+var globule = require('globule');
+var shelljs = require('shelljs');
 
 var ArchetypeJekyllGenerator = module.exports = function ArchetypeJekyllGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+  var dependenciesInstalled = ['bundle', 'ruby'].every(function (depend) {
+    return shelljs.which(depend);
   });
 
+  // Exit if Ruby dependencies aren't installed
+  if (!dependenciesInstalled) {
+    console.log('Looks like you\'re missing some dependencies.' +
+      '\nMake sure ' + chalk.white('Ruby') + ' and the ' + chalk.white('Bundler gem') + ' are installed, then run again.');
+    shelljs.exit(1);
+  }
+
+  // Get static info for templating
+  this.appname = path.basename(process.cwd());
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+  this.gitInfo = {
+    name: this.user.git.username,
+    email: this.user.git.email,
+    github: shelljs.exec('git config github.user', {silent: true}).output.replace(/\n/g, ''),
+  };
+
+  this.on('end', function () {
+    // Clean up temp files
+    spawn('rm', ['-r', '.jekyll'], { stdio: 'inherit' });
+
+    // Install Grunt and Bower dependencies
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
 };
 
 util.inherits(ArchetypeJekyllGenerator, yeoman.generators.Base);
 
-ArchetypeJekyllGenerator.prototype.askFor = function askFor() {
+// User input
+ArchetypeJekyllGenerator.prototype.askForUser = function askForUser() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
   console.log(this.yeoman);
+  console.log(chalk.yellow.bold('This generator will scaffold and wire a Jekyll site with Archetype. Yo, Jekyll-Archetype!') +
+    chalk.yellow('\n\nTell us a little about yourself.') + ' ☛');
 
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    name: 'author',
+    message: 'Name',
+    default: this.gitInfo.name
+  },
+  {
+    name: 'email',
+    message: 'Email',
+    default: this.gitInfo.email
+  },
+  {
+    name: 'github',
+    message: 'GitHub username',
+    default: this.gitInfo.github
+  },
+  {
+    name: 'twitter',
+    message: 'Twitter username',
+    default: '@' + this.gitInfo.github
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+
+    this.author  = props.author;
+    this.email   = props.email;
+    this.github  = props.github;
+    this.twitter = props.twitter[0] === '@' ? props.twitter.substr(1) : props.twitter;
 
     cb();
   }.bind(this));
 };
 
-ArchetypeJekyllGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
+/*ArchetypeJekyllGenerator.prototype.askForTools = function askForTools() {
+  var cb = this.async();
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+  console.log(chalk.yellow('\nWire tools and preprocessors.') + ' ☛');
+
+  var prompts = [{
+    name: 'cssPre',
+    type: 'list',
+    message: 'CSS preprocessor',
+    choices: ['None', 'Sass', 'Compass']
+  }];
+
+  this.prompt(prompts, function (props) {
+
+    // Multiple choice 'None' to false
+    this.cssPre  = props.cssPre === 'None' ? false : props.cssPre.toLowerCase();
+
+    cb();
+  }.bind(this));
+};*/
+
+ArchetypeJekyllGenerator.prototype.askForStructure = function askForStructure() {
+  var cb = this.async();
+
+  console.log(chalk.yellow('\nSet up some directories.') + ' ☛' +
+    '\nNested directories are fine.');
+
+  var slashFilter = function (input) {
+    return input.replace(/^\/*|\/*$/g, '');
+  };
+
+  var prompts = [{
+    name: 'cssDir',
+    message: 'CSS directory',
+    default: 'css',
+    filter: slashFilter
+  },
+  {
+    name: 'jsDir',
+    message: 'Javascript directory',
+    default: 'js',
+    filter: slashFilter
+  },
+  {
+    name: 'imgDir',
+    message: 'Image directory',
+    default: 'img',
+    filter: slashFilter
+  },
+  {
+    name: 'fontsDir',
+    message: 'Webfont directory',
+    default: 'fonts',
+    filter: slashFilter
+  }];
+  var cssPreDirPrompt = {
+    name: 'cssPreDir',
+    message: 'CSS preprocessor directory',
+    default: '_scss',
+    filter: slashFilter
+  };
+
+  if (this.cssPre) {
+    prompts.push(cssPreDirPrompt);
+  }
+
+  this.prompt(prompts, function (props) {
+
+    this.cssDir    = props.cssDir;
+    this.jsDir     = props.jsDir;
+    this.imgDir    = props.imgDir;
+    this.fontsDir  = props.fontsDir;
+    this.cssPreDir = props.cssPreDir;
+
+    cb();
+  }.bind(this));
 };
 
-ArchetypeJekyllGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
+ArchetypeJekyllGenerator.prototype.askForTemplates = function askForTemplates() {
+  var cb = this.async();
+
+  console.log(chalk.yellow('\nChoose a template.') + ' ☛');
+
+  var prompts = [{
+    name: 'templateType',
+    type: 'list',
+    message: 'Site template',
+    choices: ['Default Jekyll', 'HTML5 ★ Boilerplate']
+  }];
+
+  this.prompt(prompts, function (props) {
+
+    if (props.templateType === 'Default Jekyll') {
+      this.templateType = 'default';
+    }
+    else if (props.templateType === 'HTML5 ★ Boilerplate') {
+      this.templateType = 'h5bp';
+    }
+
+    cb();
+  }.bind(this));
+};
+
+ArchetypeJekyllGenerator.prototype.askForh5bp = function askForh5bp() {
+  if (this.templateType === 'h5bp') {
+    var cb = this.async();
+
+    var prompts = [{
+      name: 'h5bpCss',
+      type: 'confirm',
+      message: 'Add H5★BP CSS files?'
+    },
+    {
+      name: 'h5bpJs',
+      type: 'confirm',
+      message: 'Add H5★BP javascript files?',
+    },
+    {
+      name: 'h5bpIco',
+      type: 'confirm',
+      message: 'Add H5★BP favorite and touch icons?'
+    },
+    {
+      name: 'h5bpDocs',
+      type: 'confirm',
+      message: 'Add H5★BP documentation?'
+    },
+    {
+      name: 'h5bpAnalytics',
+      type: 'confirm',
+      message: 'Include Google Analytics?'
+    }];
+
+    this.prompt(prompts, function (props) {
+
+      this.h5bpCss       = props.h5bpCss;
+      this.h5bpJs        = props.h5bpJs;
+      this.h5bpIco       = props.h5bpIco;
+      this.h5bpDocs      = props.h5bpDocs;
+      this.h5bpAnalytics = props.h5bpAnalytics;
+
+      cb();
+    }.bind(this));
+  }
+  else {
+    this.h5bpCss       = false;
+    this.h5bpJs        = false;
+    this.h5bpIco       = false;
+    this.h5bpDocs      = false;
+    this.h5bpAnalytics = false;
+  }
+};
+
+ArchetypeJekyllGenerator.prototype.askForJekyll = function askForJekyll() {
+  var cb = this.async();
+
+  console.log(chalk.yellow('\nAnd finally, configure Jekyll.') + ' ☛' +
+              '\nYou can change all of these options in _config.yml.');
+
+  var prompts = [{
+    name: 'jekDescript',
+    message: 'Site description'
+  },
+  {
+    name: 'jekPost',
+    type: 'list',
+    message: 'Post permalink style',
+    choices: ['date', 'pretty', 'none']
+  },
+  {
+    name: 'jekMkd',
+    type: 'list',
+    message: 'Markdown library',
+    choices: ['redcarpet', 'maruku', 'rdiscount', 'kramdown']
+  },
+  {
+    name: 'jekPyg',
+    type: 'confirm',
+    message: 'Use the Pygments code highlighting library?'
+  },
+  {
+    name: 'jekPage',
+    message: 'Number of posts to show on the home page',
+    default: 'all',
+    validate: function (input) {
+      if (/^[0-9]*$/.test(input)) {
+        return true;
+      }
+      if (/^all*$/i.test(input)) {
+        return true;
+      }
+      return 'Must be a number or \'all\'';
+    }
+  }];
+
+  this.prompt(prompts, function (props) {
+
+    this.jekPyg      = props.jekPyg;
+    this.jekMkd      = props.jekMkd;
+    this.jekPost     = props.jekPost;
+    this.jekDescript = props.jekDescript;
+    this.jekPage     = /^all$/i.test(props.jekPage) ? false : props.jekPage;
+
+    cb();
+  }.bind(this));
+};
+
+// Generate App
+ArchetypeJekyllGenerator.prototype.rubyDependencies = function rubyDependencies() {
+  this.template('Gemfile');
+  this.conflicter.resolve(function (err) {
+    if (err) {
+      return this.emit('error', err);
+    }
+    shelljs.exec('bundle install');
+  });
+};
+
+ArchetypeJekyllGenerator.prototype.git = function git() {
+  this.template('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+ArchetypeJekyllGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
+};
+
+ArchetypeJekyllGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+ArchetypeJekyllGenerator.prototype.bower = function bower() {
+  this.copy('bowerrc', '.bowerrc');
+  this.template('_bower.json', 'bower.json');
+};
+
+ArchetypeJekyllGenerator.prototype.jshint = function jshint() {
   this.copy('jshintrc', '.jshintrc');
 };
+
+ArchetypeJekyllGenerator.prototype.csslint = function csslint() {
+  this.template('csslintrc', '.csslintrc');
+};
+
+ArchetypeJekyllGenerator.prototype.editor = function editor() {
+  this.copy('editorconfig', '.editorconfig');
+};
+
+ArchetypeJekyllGenerator.prototype.jekyllInit = function jekyllInit() {
+
+  // Create the default Jekyll site in a temp folder
+  this.jekTmpDir = path.join(process.cwd(), '.jekyll');
+  shelljs.exec('bundle exec jekyll new ' + this.jekTmpDir);
+};
+
+ArchetypeJekyllGenerator.prototype.templates = function templates() {
+
+  // Format date for posts
+  var date = new Date();
+  var formattedDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+
+  // Scaffold Jekyll dirs
+  this.mkdir('app/_layouts');
+  this.mkdir('app/_posts');
+  this.mkdir('app/_includes');
+  this.mkdir('app/_plugins');
+  this.mkdir(path.join('app', this.cssDir));
+  this.mkdir(path.join('app', this.jsDir));
+  this.mkdir(path.join('app', this.imgDir));
+  this.mkdir(path.join('app', this.fontsDir));
+
+  // Jekyll config files
+  this.copy('_config.build.yml', '_config.build.yml');
+  this.template('_config.yml');
+
+  // Project posts
+  this.copy(path.join(this.jekTmpDir, '_posts', formattedDate + '-welcome-to-jekyll.markdown'), path.join('app/_posts', formattedDate + '-welcome-to-jekyll.md'));
+  this.template('app/_posts/yo-jekyllrb.md', 'app/_posts/' + formattedDate + '-yo-jekyllrb.md');
+
+  // Jekyll default template
+  if (this.templateType === 'default') {
+
+    // Default Jekyll files
+    this.copy(path.join(this.jekTmpDir, 'index.html'), 'app/index.html');
+    this.copy(path.join(this.jekTmpDir, '_layouts/post.html'), 'app/_layouts/post.html');
+    this.copy(path.join(this.jekTmpDir, 'css/main.css'), path.join('app', this.cssDir, 'main.css'));
+
+    // Jekyll files tailored for Yeoman
+    this.template('conditional/template-default/_layouts/default.html', 'app/_layouts/default.html');
+    // Empty file for Usemin defaults
+    this.write(path.join('app', this.jsDir, 'main.js'), '');
+  }
+
+  // HTML5 Boilerplate template
+  else if (this.templateType === 'h5bp') {
+    var cb = this.async();
+
+    // H5BP files tailored for Yeoman and Jekyll
+    this.copy('conditional/template-h5bp/index.html', 'app/index.html');
+    this.copy('conditional/template-h5bp/_layouts/post.html', 'app/_layouts/post.html');
+    this.template('conditional/template-h5bp/humans.txt', 'app/humans.txt');
+    this.template('conditional/template-h5bp/_includes/scripts.html', 'app/_includes/scripts.html');
+    this.template('conditional/template-h5bp/_layouts/default.html', 'app/_layouts/default.html');
+
+    // Google analytics include
+    if (this.h5bpAnalytics) {
+      this.copy('conditional/template-h5bp/_includes/googleanalytics.html', 'app/_includes/googleanalytics.html');
+    }
+
+    // Pull H5BP in from Github
+    // Use a pre-release commit because there's so much good stuff in it.
+    this.remote('h5bp', 'html5-boilerplate', '23f5e084e559177b434f702ff6be1d83e66374d3', function (err, remote) {
+      if (err) {
+        return cb(err);
+      }
+
+      // Always include files
+      remote.copy('.htaccess', 'app/.htaccess');
+      remote.copy('404.html', 'app/404.html');
+      remote.copy('crossdomain.xml', 'app/crossdomain.xml');
+      remote.copy('LICENSE.md', 'app/_h5bp-docs/LICENSE.md');
+      remote.copy('robots.txt', 'app/robots.txt');
+
+      // CSS boilerplate
+      if (this.h5bpCss) {
+        remote.copy('css/main.css', path.join('app', this.cssDir, 'main.css'));
+      }
+      else {
+        // Empty file
+        this.write(path.join('app', this.cssDir, 'main.css'), '');
+      }
+
+      // Js boilerplate
+      // Vendor javascript is handled by Bower
+      if (this.h5bpJs) {
+        remote.copy('js/main.js', path.join('app', this.jsDir, 'main.js'));
+        remote.copy('js/plugins.js', path.join('app', this.jsDir, 'plugins.js'));
+      }
+      else {
+        // Empty file
+        this.write(path.join('app', this.jsDir, 'main.js'), '');
+      }
+
+      // Touch and favicon
+      if (this.h5bpIco) {
+        remote.copy('apple-touch-icon-144x144-precomposed.png', 'app/apple-touch-icon-precomposed.png');
+        remote.copy('favicon.ico', 'app/favicon.ico');
+      }
+
+      // Docs
+      if (this.h5bpDocs) {
+        remote.directory('doc', 'app/_h5bp-docs/code-docs');
+        remote.copy('CHANGELOG.md', 'app/_h5bp-docs/CHANGELOG.md');
+        remote.copy('CONTRIBUTING.md', 'app/_h5bp-docs/CONTRIBUTING.md');
+        remote.copy('README.md', 'app/_h5bp-docs/README.md');
+      }
+
+      cb();
+    }.bind(this));
+  }
+};
+
+ArchetypeJekyllGenerator.prototype.pygments = function pygments() {
+
+  // Pygments styles
+  if (this.jekPyg) {
+    this.copy(path.join(this.jekTmpDir, 'css/syntax.css'), path.join('app', this.cssDir, 'syntax.css'));
+  }
+}
+
+ArchetypeJekyllGenerator.prototype.cssPreprocessor = function cssPreprocessor() {
+  if (this.cssPre) {
+    this.mkdir(path.join('app', this.cssPreDir));
+  }
+
+  // Sass and Compass
+  if (['sass', 'compass'].indexOf(this.cssPre) !== -1) {
+
+    this.template('conditional/sass-compass/readme.md', path.join('app', this.cssPreDir, 'readme.md'));
+
+    // Copy CSS files to SCSS
+    var files = globule.find('**/*.css', {srcBase: path.join('app', this.cssDir)});
+    var cssDir = this.cssDir;
+    var cssDir = this.cssDir;
+
+    files.forEach(function (file) {
+
+      this.copy(path.join(process.cwd(), 'app', cssDir, file),
+                path.join('app', this.cssPreDir, file.replace(/\.css$/, '.scss')));
+
+      // Wait until copy is completely finished and then delete files.
+      this.conflicter.resolve(function (err) {
+        if (err) {
+          return this.emit('error', err);
+        }
+        spawn('rm', [path.join('app', cssDir, file)], { stdio: 'inherit' });
+      });
+    }, this);
+  }
+};
+
+ArchetypeGenerator.prototype.archetype = function app() { 
+  var cb = this.async();
+
+  // Get Archetype and provide a "remote" object as a facade API
+  this.remote('kwaledesign', 'Archetype', function(err, remote) {
+    if (err) {
+      return cb(err);
+    }
+    
+    // Get config.rb file from templates/ 
+    // (not done remotely to allow lo-dash template for directory names)
+    this.template('_config.rb', 'app/config.rb');
+
+    // Archetype screen.scss
+    remote.template('sass/screen.scss', path.join('app', this.cssPreDir, 'screen.scss')); 
+
+    // Archetype Base
+    remote.directory('sass/base/', path.join('app', this.cssPreDir, 'base')); 
+    // Archetype Objects  
+    remote.directory('sass/objects/', path.join('app', this.cssPreDir, 'objects')); 
+    // Archetype Components
+    remote.directory('sass/components/', path.join('app', this.cssPreDir, 'components')); 
+    // Archetype Layout
+    remote.directory('sass/layout/', path.join('app', this.cssPreDir, 'layout')); 
+    // Archetype Temp
+    remote.directory('sass/temp/', path.join('app', this.cssPreDir, 'temp')); 
+
+    // Archetype Docs (Styleguide and Pattern Library powered by Dexy)
+    remote.directory('docs', 'app/docs');
+
+    cb(); 
+  }.bind(this));
+};
+
+
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,372 @@
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+// Directory reference:
+//   css: <%= cssDir %>
+//   sass: <%= cssPreDir %>
+//   javascript: <%= jsDir %>
+//   images: <%= imgDir %>
+//   fonts: <%= fontsDir %>
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var yeomanConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    watch: {<% if (cssPre) { %>
+      <%= cssPre %>: {
+        files: ['<%%= yeoman.app %>/<%= cssPreDir %>/**/*.{scss,sass}'],
+        tasks: ['<%= cssPre %>:server']
+      prefixCss: {
+        files: ['<%%= yeoman.app %>/<%= cssDir %>/**/*.css'],
+        tasks: ['copy:stageCss']
+      },
+     jekyll: {
+        files: [
+          '<%%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}',
+          '_config.yml',
+          '!<%%= yeoman.app %>/bower_components'
+        ],
+        tasks: ['jekyll:server']
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          '.jekyll/**/*.html',
+          '{.tmp,<%%= yeoman.app %>}/<%= cssDir %>/**/*.css',
+          '{.tmp,<%%= yeoman.app %>}/<%%= js %>/**/*.js',
+          '<%%= yeoman.app %>/<%= imgDir %>/**/*.{gif,jpg,jpeg,png,svg,webp}'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // Change hostname to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, '.jekyll'),
+              mountFolder(connect, yeomanConfig.app)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, yeomanConfig.dist)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test'),
+              mountFolder(connect, yeomanConfig.app)
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        path: 'http://localhost:<%%= connect.options.port %>'
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%%= yeoman.dist %>/*',
+            // Running Jekyll also cleans the target directory.  Exclude any
+            // non-standard `keep_files` here (e.g., the generated files
+            // directory from Jekyll Picture Tag).
+            '!<%%= yeoman.dist %>/.git*'
+          ]
+        }]
+      },
+      server: ['.tmp', '.jekyll']
+    },
+    compass: {
+      options: {
+        // If you're using global Sass gems, require them here.
+        // require: ['singularity', 'jacket'],
+        bundleExec: true,
+        sassDir: '<%%= yeoman.app %>/<%= cssPreDir %>',
+        cssDir: '.tmp/<%= cssDir %>',
+        imagesDir: '<%%= yeoman.app %>/<%= imgDir %>',
+        javascriptsDir: '<%%= yeoman.app %>/<%= jsDir %>',
+        relativeAssets: false,
+        httpImagesPath: '/<%= imgDir %>',
+        httpGeneratedImagesPath: '/<%= imgDir %>/generated',
+        outputStyle: 'expanded',
+        raw: 'extensions_dir = "<%%= yeoman.app %>/bower_components"\n'
+      },
+      dist: {
+        options: {
+          generatedImagesDir: '<%%= yeoman.dist %>/<%= imgDir %>/generated'
+        }
+      },
+      server: {
+        options: {
+          debugInfo: true,
+          generatedImagesDir: '.tmp/<%= imgDir %>/generated'
+        }
+      }
+    },
+    jekyll: {
+      options: {
+        bundleExec: true,
+        config: '_config.yml',
+        src: '<%%= yeoman.app %>'
+      },
+      dist: {
+        options: {
+          dest: '<%%= yeoman.dist %>',
+          config: '_config.yml,_config.build.yml'
+        }
+      },
+      server: {
+        options: {
+          dest: '.jekyll',
+        }
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        '<%%= yeoman.app %>/<%= jsDir %>/**/*.js',
+        'test/spec/**/*.js',
+        '!<%%= yeoman.app %>/<%= jsDir %>/vendor/**/*',
+        '!<%%= yeoman.app %>/bower_components/**/*'
+      ]
+    },
+    csscss: {
+      options: {
+        bundleExec: true,
+        minMatch: 2,
+        ignoreSassMixins: false,
+        compass: true,
+        showParserErrors: true,
+        colorize: true,
+        shorthand: false,
+        verbose: true
+      },
+      check: {
+       src: ['<%%= yeoman.app %>/<%= cssDir %>/**/*.css', 
+             '<%%= yeoman.app %>/<%= cssPreDir %>/**/*.scss']
+      }
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      check: {
+        src: [
+          '<%%= yeoman.app %>}/<%= cssDir %>/**/*.css',
+          '<%%= yeoman.app %>}/<%= cssPreDir %>/**/*.scss'
+        ]
+      }
+    },
+    // UseminPrepare will only scan a single page for usemin blocks. If you
+    // use usemin blocks that aren't in index.html, create a usemin manifest
+    // page (hackery!) and point this task there.
+    useminPrepare: {
+      options: {
+        dest: '<%%= yeoman.dist %>'
+      },
+      html: '<%%= yeoman.dist %>/index.html'
+    },
+    usemin: {
+      options: {
+          basedir: '<%%= yeoman.dist %>',
+          dirs: ['<%%= yeoman.dist %>/**/*']
+      },
+      html: ['<%%= yeoman.dist %>/**/*.html'],
+      css: ['<%%= yeoman.dist %>/<%= cssDir %>/**/*.css']
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%%= yeoman.dist %>',
+          src: '**/*.html',
+          dest: '<%%= yeoman.dist %>'
+        }]
+      }
+    },
+    // Usemin adds files to concat
+    concat: {},
+    // Usemin adds files to uglify
+    uglify: {},
+    // Usemin adds files to cssmin
+    cssmin: {
+      dist: {
+        options: {
+          check: 'gzip'
+        }
+      }
+    },
+    imagemin: {
+      dist: {
+        options: {
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%%= yeoman.dist %>',
+          src: '**/*.{jpg,jpeg,png}',
+          dest: '<%%= yeoman.dist %>'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%%= yeoman.dist %>',
+          src: '**/*.svg',
+          dest: '<%%= yeoman.dist %>'
+        }]
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%%= yeoman.app %>',
+          src: [
+            // Jekyll processes and moves HTML and text files. Usemin processes
+            // and moves CSS and javascript inside of Usemin blocks.
+            // Copy moves asset files and directories
+            '*.{ico,png}',
+            '<%= imgDir %>/**/*',
+            '<%= fontsDir %>/**/*',
+            // Follow the Jekyll pattern and exclude all files and folders
+            // prefixed with an underscore
+            '!**/_*/**',
+            '!**/_*'<% if (h5bpJs) { %>,<% } %>
+            // Explicitly add other files your site needs for distribution here
+            <% if (!h5bpJs) { %>// <% } %>'bower_components/jquery.min.js'
+          ],
+          dest: '<%%= yeoman.dist %>'
+        }]
+      }
+    },
+    rev: {
+      options: {
+        length: 4
+      },
+      dist: {
+        files: {
+          src: [
+            '<%%= yeoman.dist %>/<%= jsDir %>/**/*.js',
+            '<%%= yeoman.dist %>/<%= cssDir %>/**/*.css',
+            '<%%= yeoman.dist %>/<%= imgDir %>/**/*.{gif,jpg,jpeg,png,svg,webp}',
+            '<%%= yeoman.dist %>/<%= fontsDir %>/**/*.{eot*,otf,svg,ttf,woff}'
+          ]
+        }
+      }
+    },
+    concurrent: {
+      server: [
+        'compass:server',
+        'jekyll:server'
+      ],
+      dist: [
+        'compass:dist',
+        'copy:dist'
+      ]
+    }
+  });
+
+  // Define Tasks
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'concurrent:server',
+      'connect:livereload',
+      'open',
+      'watch'
+    ]);
+  });
+
+  // No real tests yet. Add your own.
+  grunt.registerTask('test', [
+  //   'clean:server',
+  //   'concurrent:test',
+  //   'connect:test'
+  ]);
+
+  grunt.registerTask('check', [
+    'clean:server',
+    'compass:server',
+    'jshint:all',
+    'csscss:check',
+    'csslint:check'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    // Jekyll cleans files from the target directory, so must run first
+    'jekyll:dist',
+    'concurrent:dist',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'imagemin',
+    'svgmin',
+    'rev',
+    'usemin',
+    'htmlmin'
+    ]);
+
+  grunt.registerTask('default', [
+    'check',
+    'test',
+    'build'
+  ]);
+};
